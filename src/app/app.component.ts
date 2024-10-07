1e2a3da1e12db0e6ec85712ddb199888f535bddf
nakamuuu-prog <affilimoney2@gmail.com>
2024-10-07 14:06:23 +0900

    デザインの作り込み

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Inject,
+  ElementRef,
+  ViewChild,
+  Renderer2,
+} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { Location, DOCUMENT } from '@angular/common';
+import { NavbarComponent } from './common/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'reservation-app';
+export class AppComponent implements OnInit {
+  private _router!: Subscription;
+  @ViewChild(NavbarComponent) navbar!: NavbarComponent;
+
+  constructor(
+    private renderer: Renderer2,
+    private router: Router,
+    @Inject(DOCUMENT) private document: any,
+    private element: ElementRef,
+    public location: Location
+  ) {}
+
+  ngOnInit() {
+    var navbar: HTMLElement =
+      this.element.nativeElement.children[0].children[0];
+    this._router = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.navbar.sidebarClose();
+      });
+    this.renderer.listen('window', 'scroll', (event) => {
+      const number = window.scrollY;
+      if (number > 150 || window.pageYOffset > 150) {
+        // add logic
+        navbar.classList.remove('navbar-transparent');
+      } else {
+        // remove logic
+        navbar.classList.add('navbar-transparent');
+      }
+    });
+  }
 }
